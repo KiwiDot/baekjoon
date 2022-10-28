dba7a9ea91cadc548a563a19995b14ba06402b94
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2022-10-28 10:14:19 +0900

    [파이썬] 백준 25643번 문자열 탑 쌓기
    2022-10-28

---
+++ 25643번 문자열 탑 쌓기/boj_25643.py
@@ -0,0 +1,24 @@
+# 백준 25643번 문자열 탑 쌓기
+import sys
+put = sys.stdin.readline
+
+N, M = map(int, put().split())
+S = put().strip()
+check = True
+
+for i in range(N-1):
+    s = put().strip()
+    check2 = False
+
+    for m in range(1, M+1):
+        if S[:m] == s[-m:] or S[-m:] == s[:m]:
+            check2 = True
+            break
+
+    if check2:
+        S = s
+    else:
+        check = False
+        break
+
+print(1 if check else 0)
