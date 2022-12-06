66c3d27225108b00507d3fc1f8c1a2d8c0fc4123
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2022-12-06 15:39:24 +0900

    -
    2022-12-06

---
+++ 1000 ~ 1999/1730번 판화/boj1730.py
@@ -0,0 +1,42 @@
+# 백준 1730번 판화
+import sys
+put = sys.stdin.readline
+
+
+def moving(x, y, d1, d2):
+    if answer[x][y] == '+':
+        pass
+    elif answer[x][y] == d2:
+        answer[x][y] = '+'
+    else:
+        answer[x][y] = d1
+
+
+N = int(put())
+move = put().strip()
+answer = [['.' for i in range(N)] for j in range(N)]
+x = y = 0
+
+for i in move:
+    if i == 'U' and x > 0:
+        moving(x, y, '|', '-')
+        x -= 1
+        moving(x, y, '|', '-')
+
+    if i == 'D' and x < N - 1:
+        moving(x, y, '|', '-')
+        x += 1
+        moving(x, y, '|', '-')
+
+    if i == 'L' and y > 0:
+        moving(x, y, '-', '|')
+        y -= 1
+        moving(x, y, '-', '|')
+
+    elif i == 'R' and y < N - 1:
+        moving(x, y, '-', '|')
+        y += 1
+        moving(x, y, '-', '|')
+
+for i in answer:
+    print(''.join(i))
