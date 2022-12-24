e1e3819860fa6df87cf4e9c9b636082f67314693
KiwiDot <116772632+KiwiDot@users.noreply.github.com>
2022-12-24 14:50:35 +0900

    -
    2022-12-24

---
+++ 26000 ~ 26999/26766번 Serca/boj26766.py
@@ -0,0 +1,17 @@
+# 백준 26766번 Serca
+import sys
+put = sys.stdin.readline
+
+N = int(put())
+
+while N:
+    N -= 1
+    print(""" @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     """)
